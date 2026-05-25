7d4e7c813a1772c2d40f0ff78c5d73f2e31f81b8
Abba Ado Bawa <inuwashadow55@gmail.com>
2026-05-25 02:50:17 +0100

    fix: update router and fix pages navigation issues

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,10 +2,37 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Login from '../pages/Login.vue'
 import AdminUsers from '../pages/AdminUsers.vue'
+import StudentDashboard from '../pages/StudentDashboard.vue'
+import Home from '../pages/Home.vue'
+import FreeTraining from '../pages/FreeTraining.vue'
+import PremiumTraining from '../pages/PremiumTraining.vue'
+import Blogs from '../pages/Blogs.vue'
+import Company from '../pages/Company.vue'
+import Complaints from '../pages/Complaints.vue'
+import BotSupport from '../pages/BotSupport.vue'
+import Contact from '../pages/Contact.vue'
 
 const routes = [
-  { path: '/', component: Login },
+  { path: '/', component: Home },
+
+  { path: '/free-training', component: FreeTraining },
+
+  { path: '/premium-training', component: PremiumTraining },
+
+  { path: '/blogs', component: Blogs },
+
+  { path: '/contact', component: Contact },
+
+  { path: '/complaints', component: Complaints },
+
+  { path: '/bot-support', component: BotSupport },
+
+  { path: '/company', component: Company },
+  
+
   { path: '/admin', component: AdminUsers },
+
+  { path: '/student-dashboard', component: StudentDashboard },
 ]
 
 const router = createRouter({
