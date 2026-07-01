9a03ae15003f65f8c64356b888c77cee313c3f6e
Abba Ado Bawa <inuwashadow55@gmail.com>
2026-07-01 18:04:53 +0100

    Configure CORS for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,18 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://shadowzee123.github.io",
+      "https://shadowzee123.github.io/yckf-starter"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
